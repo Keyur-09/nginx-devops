69ef67f18508f912eec24222ee0208629c340163
Keyur-09 <keyur.kyada@corenettechltd.com>
2026-02-02 17:45:30 +0530

    Day-1: Initial FastAPI app with Docker setup

---
+++ apps/app1/src/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from datetime import datetime, timezone
+
+app = FastAPI()
+
+@app.get("/health")
+def health():
+    return{"status": "ok"}
+
+@app.get("/time")
+def time():
+    return{
+        "time": datetime.now(timezone.utc).isoformat()
+    }
